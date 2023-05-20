c829960f6fddd7a7e5b957871f406accd10d864b
Adam Chu <adamchu@sfu.ca>
2023-05-20 16:25:56 -0700

    v1.1

--- fireblade_hw.py
+++ fireblade_hw.py
@@ -2,18 +2,41 @@
 
 from jnpr.junos import Device
 
-# virtual-chassis type
-def chassis_type(dev):
+# get hardware info
+def hw_dict(dev):
+    return dev.facts
 
-    # get hardware info
-    hostname = dev.facts['hostname']
-    model_info = dev.facts['model_info']
+# chassis type for JUNOS installation purpose
+def chassis(dev):
+
+    model_info = hw_dict(dev)['model_info']
 
     models = set(model_info.values())
 
     if len(models) == 1:
-        chassis_type = models.pop()
+        chassis = models.pop()
     else:
-        chassis_type = 'mixed'
+        chassis = 'mixed'
     
-    return chassis_type
+    return chassis
+
+# routing engine model
+def model(dev):
+    return hw_dict(dev)['model']
+
+# chassis role
+def role(dev):
+    h = hw_dict(dev)['hostname'].lower()
+    r = 'edge' if h.find('edge') != -1 else 'core' if h.find('core') != -1 else 'ext' if h.find('ext') != -1 else None
+    return r
+
+# campus info
+def campus(dev):
+    h = hw_dict(dev)['hostname'].lower()
+    campus = h.split('-')[0]
+    return campus
+
+# building info
+def bld(dev):
+    h = hw_dict(dev)['hostname'].lower()
+    bld = h.split('-')[1]
